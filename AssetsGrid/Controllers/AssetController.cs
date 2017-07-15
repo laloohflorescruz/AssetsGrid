8f43867f3e3cf8c3452c5454c7fc78c360585876
Lalo Flores Cruz <eflores@crudcreativo.com>
2017-07-15 19:25:27 -0400

    First commit

---
+++ AssetsGrid/Controllers/AssetController.cs
@@ -0,0 +1,43 @@
+﻿using AssetsGrid.Models;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetsGrid.Controllers
+{
+    
+    public class AssetController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ApplicationDbContext DbContext
+        {
+            get
+            {
+                return _dbContext ?? HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+            }
+            private set
+            {
+                _dbContext = value;
+            }
+
+        }
+        public AssetController()
+        {
+
+        }
+
+        public AssetController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: Asset
+        public ActionResult Index()
+        {
+            return View(DbContext.Assets.ToList());
+        }
+    }
+}
